82da2b5e14ed08e536dd4380f06e3b2105415948
ahmedos1999 <ah_os5@yahoo.com>
2023-10-17 01:45:14 +0200

    PY o

---
+++ 0x0C-python-almost_a_circle/5-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 5-main """
+import models.rectangle
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(4, 6, 2, 1, 12)
+    print(r1)
+
+    r2 = Rectangle(5, 5, 1)
+    print(r2)
